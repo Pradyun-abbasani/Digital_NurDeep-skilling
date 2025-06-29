30d22411a6a318897efbe585bafe601abbab12e2
Pradyun-abbasani <pradyunabbasani@gmail.com>
2025-06-29 19:45:24 +0530

    Delete WEEK-2/PlSql directory

--- WEEK-2/PlSql/Exercise 1 Control Structures/Controlstructure1.sql.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-  FOR loan_rec IN (SELECT LoanID, InterestRate FROM Loans l JOIN Customers c ON l.CustomerID = c.CustomerID WHERE FLOOR(MONTHS_BETWEEN(SYSDATE, DOB)/12) > 60) LOOP
-    UPDATE Loans
-    SET InterestRate = InterestRate - 1
-    WHERE LoanID = loan_rec.LoanID;
-  END LOOP;
-  COMMIT;
-END;
-/
--- WEEK-2/PlSql/Exercise 5 Triggers/LogTransaction.sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER LogTransaction
-AFTER INSERT ON Transactions
-FOR EACH ROW
-BEGIN
-  INSERT INTO AuditLog (TransactionID, LogDate, Message)
-  VALUES (:NEW.TransactionID, SYSDATE, 'Transaction inserted.');
-END;
-/
